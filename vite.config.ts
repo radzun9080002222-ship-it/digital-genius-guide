fd3992585c20d3007cea9d57a55f49169d076f78
radzun9080002222-ship-it <radzun9080002222@gmail.com>
2025-12-19 14:32:47 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/", // ✅ важно для деплоя на собственный домен
   server: {
     host: "::",
     port: 8080,
